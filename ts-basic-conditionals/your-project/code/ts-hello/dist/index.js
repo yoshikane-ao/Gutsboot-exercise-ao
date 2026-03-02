7e189bf34bac26cf03495dd14d34dc4265727987
ao <popoponpop@gmail.com>
2026-03-02 13:56:26 +0900

    feat: add conditionals and comparisons

---
+++ ts-basic-conditionals/your-project/code/ts-hello/dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const score = 50;
+if (score <= 60) {
+    console.log("pass");
+}
+else {
+    console.log("fail");
+}
+//二問目
+const age = 27;
+const isweekend = true;
+if (age >= 20 && isweekend) {
+    console.log("You can drink");
+}
+else {
+    console.log("You cannot drink");
+}
+const ismember = true;
+const hascoupon = true;
+if (ismember || hascoupon) {
+    console.log("Discount");
+}
+else {
+    console.log("No discount");
+}
+//# sourceMappingURL=index.js.map
---
+++ ts-basic-conditionals/your-project/code/ts-hello/src/index.ts
@@ -0,0 +1,24 @@
+const score: number = 50;
+ if (score <= 60) {
+    console.log("pass");
+ } else {
+    console.log("fail")
+ }
+
+ //二問目
+
+const age: number = 27;
+const isweekend: boolean = true;
+if (age >= 20 && isweekend) {
+    console.log("You can drink")
+} else {
+    console.log("You cannot drink")
+}
+
+const ismember: boolean = true;
+const hascoupon: boolean = true;
+if (ismember || hascoupon) {
+    console.log("Discount");
+} else {
+    console.log("No discount");
+}

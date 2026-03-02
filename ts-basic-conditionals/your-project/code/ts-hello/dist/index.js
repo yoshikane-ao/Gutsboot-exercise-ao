30bf17186d28e5f7998f60bff033fdfdd59ce19e
ao <popoponpop@gmail.com>
2026-03-02 13:59:25 +0900

    feat: add conditionals and comparisons

--- ts-basic-conditionals/your-project/code/ts-hello/dist/index.js
+++ ts-basic-conditionals/your-project/code/ts-hello/dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const score = 50;
-if (score <= 60) {
+//一問目
+const score = 70;
+if (score >= 60) {
     console.log("pass");
 }
 else {
@@ -10,12 +11,13 @@ else {
 //二問目
 const age = 27;
 const isweekend = true;
-if (age >= 20 && isweekend) {
+if (age >= 20 && isweekend === true) {
     console.log("You can drink");
 }
 else {
     console.log("You cannot drink");
 }
+//三問目
 const ismember = true;
 const hascoupon = true;
 if (ismember || hascoupon) {
--- ts-basic-conditionals/your-project/code/ts-hello/src/index.ts
+++ ts-basic-conditionals/your-project/code/ts-hello/src/index.ts
@@ -1,20 +1,21 @@
-const score: number = 50;
- if (score <= 60) {
+//一問目
+const score: number = 70;
+ if (score >= 60) {
     console.log("pass");
  } else {
     console.log("fail")
  }
 
  //二問目
-
 const age: number = 27;
 const isweekend: boolean = true;
-if (age >= 20 && isweekend) {
+if (age >= 20 && isweekend === true) {
     console.log("You can drink")
 } else {
     console.log("You cannot drink")
 }
 
+//三問目
 const ismember: boolean = true;
 const hascoupon: boolean = true;
 if (ismember || hascoupon) {

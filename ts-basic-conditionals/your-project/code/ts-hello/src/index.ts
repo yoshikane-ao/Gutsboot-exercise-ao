a894048ec62d916f6218445f49d838c49993944d
ao <popoponpop@gmail.com>
2026-03-16 21:12:56 +0900

    3月16日演習分

--- ts-basic-conditionals/your-project/code/ts-hello/src/index.ts
+++ ts-basic-conditionals/your-project/code/ts-hello/src/index.ts
@@ -1,17 +1,28 @@
 //一問目
+//scoreを定数にして、number型の70を代入する
 const score: number = 70;
+//scoreが60以上の条件のとき
  if (score >= 60) {
+  //passと出力
     console.log("pass");
+    //もし60以上出なかった時、
  } else {
+  //failと出力
     console.log("fail")
  }
 
  //二問目
+ //定数ageにnumber型の27を代入する。
 const age: number = 27;
+//定数isweekendに初期値trueのboolean型を代入する
 const isweekend: boolean = true;
+//もしageが20以上の時かつ、isweekendがtrueの時
 if (age >= 20 && isweekend === true) {
+  //You can drinkと出力
     console.log("You can drink")
+    //もし違った場合
 } else {
+  //You cannot drinkと出力
     console.log("You cannot drink")
 }
 
